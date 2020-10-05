d9b068200fe1265008cf7085cd9b1a0b93bc71a7
akashrattan43 <akashrattan43@gmail.com>
2020-10-05 13:42:46 -0700

    code

--- hello.py
+++ hello.py
@@ -1,4 +1,44 @@
-first = 'Akash'
-last = 'Rattan'
-full = f'{first} {last}'
-print(full)
+# course = 'Python for begginers'
+
+# print(course.replace("P" , "j"))
+# print("pro" in course)
+
+# imaginary numbers
+# x = 2
+# x = 4
+# x = 1 + 2j
+# print(x)
+# print(10 // 3)
+
+# abs
+# print(abs(-2.9))
+# module
+# import math
+# print(math.ceil(2.2))
+
+# input
+# x = input("x: ")
+# y = int(x) + 1
+# print(f"x: {x} y: {y}")
+
+# Conditional Statements
+# temperature = 35
+# if temperature > 40:
+#     print("It's warm")
+# elif temperature < 40:
+#     print("It's cold")
+# print('Done')
+
+# Ternary Operator
+# age = 22
+# message = 'Eligable' if age >= 18 else 'Not Eligable'
+# print(message)
+
+# Logical Operator 
+# high_income = True
+# good_credit = False
+# if high_income or good_credit: 
+#     print('Eligable')
+# else: 
+#     print('Not Eligable')
+
