d6372de6e8014a99c03652bf022e4ae728f99972
akashrattan43 <akashrattan43@gmail.com>
2020-10-06 12:44:16 -0700

    practice

--- hello.py
+++ hello.py
@@ -34,11 +34,68 @@
 # message = 'Eligable' if age >= 18 else 'Not Eligable'
 # print(message)
 
-# Logical Operator 
+# Logical Operator
 # high_income = True
 # good_credit = False
-# if high_income or good_credit: 
+# if high_income or good_credit:
 #     print('Eligable')
-# else: 
+# else:
 #     print('Not Eligable')
+# high_income = True
+# credit = True
+# student = True
+# if high_income and credit and not student:
+#     print('Eligable')
+# else:
+#     print('Not Eligable')
+
+# For loops
+# for number in range(1, 4):
+#     print('Attempt', number, number * '.')
+
+# For else
+# successfull = True
+# for number in range(3):
+#     print('Attempt')
+#     if successfull:
+#         print('Successfull')
+#         break
+# else:
+#     print('Not Successful')
+
+# Nested Loops
+# for x in range(5):
+#     for y in range(3):
+#         print(f"({x}, {y})")
+
+# Iterables
+# for x in 'Python':
+    # print(x)
+
+
+# While loop
+# number = 100
+# while number > 0:
+#     print(number)
+#     number //= 2
+#2.
+# command = ''
+# while command.lower != 'quit':
+#     command = input('>')
+#     print('ECHO ' + command)
+#infinate loop
+# while True:
+#     command = input('>')
+#     print('Echo ' + command)
+#     if command.lower() == 'quit':
+#         break
+
+#exercise 
+# count = 0
+# for x in range(1, 10):
+#     if x % 2 == 0:
+#         count += 1
+#         print(x)
+# print(f"We have {count} numbers")
+
 
